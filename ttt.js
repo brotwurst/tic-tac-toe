e65daa94510082ac55b22a1fcbe666df0775ce66
Jacob Lindahl <aviator9402@gmail.com>
2016-12-07 09:56:59 -0600

    Part 2: Game is functional

--- ttt.js
+++ ttt.js
@@ -3,18 +3,91 @@ let canvas = document.getElementById('ttt'),
     msg = document.getElementById('message'),
     cellSize = 100,
     map = [
-        1, 0, 0,
-        0, 0, -1,
-        -1, 0, 1,
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0,
     ],
     winPatterns = [
         0b111000000, 0b000111000, 0b000000111, // Rows
         0b100100100, 0b010010010, 0b001001001, // Columns
         0b100010001, 0b001010100, // Diagonals
     ],
-    BLANK = 0, X = 1, O = -1;
+    BLANK = 0, X = 1, O = -1,
+    mouse = {
+        x: -1,
+        y: -1,
+    },
+    currentPlayer = X,
+    gameOver = false;
+
 
 canvas.width = canvas.height = 3 * cellSize;
+
+canvas.addEventListener('mouseout', function () {
+    mouse.x = mouse.y = -1;
+});
+
+canvas.addEventListener('mousemove', function (e) {
+    let x = e.pageX - canvas.offsetLeft,
+        y = e.pageY - canvas.offsetTop;
+
+    mouse.x = x;
+    mouse.y = y;
+});
+
+canvas.addEventListener('click', function (e) {
+    play(getCellByCoords(mouse.x, mouse.y));
+});
+
+displayTurn();
+
+function displayTurn () {
+    msg.textContent = ((currentPlayer == X)? 'X': 'O') + '\'s turn.';
+}
+
+function play (cell) {
+    if (gameOver) return;
+
+    if (map[cell] != BLANK) {
+        msg.textContent = 'Position taken.';
+        return;
+    }
+
+    map[cell] = currentPlayer;
+
+    let winCheck = checkWin(currentPlayer);
+
+    if (winCheck != 0) {
+        gameOver = true;
+        msg.textContent = ((currentPlayer == X)? 'X': 'O') + ' wins!';
+        return;
+    } else if (map.indexOf(BLANK) == -1) {
+        gameOver = true;
+        msg.textContent = 'Tie!';
+        return;
+    }
+
+    currentPlayer *= -1;
+
+    displayTurn();
+}
+
+function checkWin (player) {
+    let playerMapBitMask = 0;
+    for (let i = 0; i < map.length; i++) {
+        playerMapBitMask <<= 1;
+        if (map[i] == player)
+            playerMapBitMask += 1;
+    }
+
+    for (let i = 0; i < winPatterns.length; i++) {
+        if ((playerMapBitMask & winPatterns[i]) == winPatterns[i]) {
+            return winPatterns[i];
+        }
+    }
+
+    return 0;
+}
 
 function draw () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -89,6 +162,10 @@ function getCellCoords (cell) {
         'x': x,
         'y': y,
     };
+}
+
+function getCellByCoords (x, y) {
+    return (Math.floor(x / cellSize) % 3) + Math.floor(y / cellSize) * 3;
 }
 
 draw();
